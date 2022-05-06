4bd559c37831ca8f0f1971483e444608fbd022d5
ccidlt <635971092@qq.com>
2022-05-06 11:25:37 +0800

    20220506

--- src/main/java/com/ds/SpringbootApplication.java
+++ src/main/java/com/ds/SpringbootApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @EnableScheduling
 @Slf4j
+@EnableFeignClients(basePackages={"com.ds.service"})
 @ServletComponentScan//检索@WebFilter、@WebListener
 @EnableSwagger2
 public class SpringbootApplication {
