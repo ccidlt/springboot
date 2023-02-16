d91162dcea71acbcb9635c505177563e8abd5b13
ccidlt <635971092@qq.com>
2023-02-16 08:54:29 +0800

    20230216

--- src/test/java/com/ds/SpringbootApplicationTests.java
+++ src/test/java/com/ds/SpringbootApplicationTests.java
@@ -1,10 +1,15 @@
 package com.ds;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.lang.tree.TreeNode;
+import cn.hutool.core.lang.tree.TreeNodeConfig;
+import cn.hutool.core.lang.tree.TreeUtil;
 import cn.hutool.core.util.*;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.http.HttpUtil;
@@ -427,6 +432,36 @@ public class SpringbootApplicationTests {
         //ServletUtil
         //Map<String, String> map = ServletUtil.getParamMap(request);
         //文件工具FileUtil
+        //树工具TreeUtil
+        // 构建node列表
+        List<TreeNode<String>> nodeList = CollUtil.newArrayList();
+        nodeList.add(new TreeNode<>("1", "0", "系统管理", 5));
+        nodeList.add(new TreeNode<>("11", "1", "用户管理", 222222));
+        nodeList.add(new TreeNode<>("111", "11", "用户添加", 0));
+        nodeList.add(new TreeNode<>("2", "0", "店铺管理", 1));
+        nodeList.add(new TreeNode<>("21", "2", "商品管理", 44));
+        nodeList.add(new TreeNode<>("221", "2", "商品管理2", 2));
+        List<Tree<String>> treeList = TreeUtil.build(nodeList, "0");
+        System.out.println("treeList:"+treeList);
+
+        //配置
+        TreeNodeConfig treeNodeConfig = new TreeNodeConfig();
+        // 自定义属性名 都要默认值的
+        treeNodeConfig.setWeightKey("order");
+        treeNodeConfig.setIdKey("rid");
+        // 最大递归深度
+        treeNodeConfig.setDeep(3);
+        //转换器
+        List<Tree<String>> treeNodes = TreeUtil.build(nodeList, "0", treeNodeConfig,
+                (treeNode, tree) -> {
+                    tree.setId(treeNode.getId());
+                    tree.setParentId(treeNode.getParentId());
+                    tree.setWeight(treeNode.getWeight());
+                    tree.setName(treeNode.getName());
+                    // 扩展属性 ...
+                    tree.putExtra("extraField", 666);
+                    tree.putExtra("other", new Object());
+                });
 
         //JWT
         Map<String, Object> map = new HashMap<String, Object>() {
