aeffbabe1bab46a4e21546aef116930dd5c304d9
ccidlt <635971092@qq.com>
2023-02-20 15:54:55 +0800

    20230220

--- src/main/java/com/ds/config/rest/RestTemplateConfig.java
+++ src/main/java/com/ds/config/rest/RestTemplateConfig.java
@@ -2,10 +2,12 @@ package com.ds.config.rest;
 
 import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.ds.entity.Result;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -21,7 +23,7 @@ public class RestTemplateConfig {
 }
 
 class ExceptionUtil {
-    public Result handleException(BlockException exception) {
-        return Result.build(503, exception.getClass().getCanonicalName()+"\t 服务不可用");
+    public static ClientHttpResponse handleException(HttpRequest request, byte[] body, ClientHttpRequestExecution execution, BlockException exception) {
+        return null;
     }
 }
