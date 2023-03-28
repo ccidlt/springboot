f9e3f49796c56a4490e1f7932df92e37f21d5db8
ccidlt <635971092@qq.com>
2023-03-28 15:23:39 +0800

    20230328

--- src/main/java/com/ds/config/async/ReentrantLockOperate.java
+++ src/main/java/com/ds/config/async/ReentrantLockOperate.java
@@ -18,6 +18,12 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ReentrantLockOperate {
 
     /**
+     * ThreadLocal叫做线程变量，意思是ThreadLocal中填充的变量属于当前线程，该变量对其他线程而言是隔离的，也就是说该变量是当前线程独有的变量。
+     * ThreadLocal为变量在每个线程中都创建了一个副本，那么每个线程可以访问自己内部的副本变量。
+     */
+    static ThreadLocal threadLocal = new ThreadLocal<String>();
+
+    /**
      * 1、作为多个线程同时使用的原子计数器。
      * addAndGet()- 以原子方式将给定值添加到当前值，并在添加后返回新值。
      * getAndAdd() - 以原子方式将给定值添加到当前值并返回旧值。
