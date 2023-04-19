a8143a1d284b46fe3c19a99e0d83b08cce242760
ccidlt <635971092@qq.com>
2023-04-19 11:51:39 +0800

    20230419

--- src/main/java/com/ds/config/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/com/ds/config/elasticsearch/ElasticsearchConfig.java
@@ -4,6 +4,7 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${spring.elasticsearch.rest.uris}")
+    private String url;
+
     @Bean
     public RestHighLevelClient restHighLevelClient(){
-        RestClientBuilder builder = RestClient.builder(HttpHost.create("http://localhost:9200"));
+        RestClientBuilder builder = RestClient.builder(HttpHost.create(url));
         return new RestHighLevelClient(builder);
     }
 
