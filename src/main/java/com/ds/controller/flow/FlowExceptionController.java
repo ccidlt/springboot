4b256ddcf30460fac1c09c595eca1c1468d08f89
ccidlt <635971092@qq.com>
2023-06-09 09:13:55 +0800

    20230609

--- src/main/java/com/ds/controller/flow/FlowExceptionController.java
+++ src/main/java/com/ds/controller/flow/FlowExceptionController.java
@@ -2,17 +2,17 @@ package com.ds.controller.flow;
 
 import com.ds.entity.Result;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Description: 异常同意处理类
  * @Author lt
  * @Date 2023/6/7 11:26
  */
-@RestControllerAdvice
 public class FlowExceptionController {
 
     @ExceptionHandler(FlowException.class)
+    @ResponseBody
      public Result flowException(FlowException ex){
          return Result.build(ex.getCode(), ex.getMessage());
      }
