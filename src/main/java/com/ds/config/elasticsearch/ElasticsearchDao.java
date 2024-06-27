c21b281c7092758e1a87220c3b36ecbf27cfae10
ccidlt <635971092@qq.com>
2024-06-27 17:28:14 +0800

    20240627

--- src/main/java/com/ds/config/elasticsearch/ElasticsearchDao.java
+++ src/main/java/com/ds/config/elasticsearch/ElasticsearchDao.java
@@ -5,7 +5,7 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.stereotype.Repository;
 
 /**
- * @Description:
+ * @Description: Elasticsearch表示实体类的类型，String表示实体类中@Id属性的类型。
  * @Author lt
  * @Date 2023/5/29 15:36
  */
